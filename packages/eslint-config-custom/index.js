91c1fcce26d94d5072cd1ab797033dc669f6401f
Sayeed Afridi <as.afridi563@gmail.com>
2023-01-26 02:06:32 +0600

    feat(*): commit, husky, eslint, prettier to improve dev env

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,23 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: ['next', 'turbo', 'prettier'],
+  plugins: ['prettier'],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
+    '@next/next/no-html-link-for-pages': 'off',
+    'react/jsx-key': 'off',
+    'jsx-quotes': ['error', 'prefer-single'],
+    quotes: [2, 'single', 'avoid-escape'],
+    'no-undef': 'error',
+    'no-console': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        bracketSpacing: true,
+        bracketSameLine: true,
+        singleQuote: true,
+        jsxSingleQuote: true,
+        trailingComma: 'all',
+        arrowParens: 'avoid',
+      },
+    ],
   },
 };
