069872ee216b970b7749a4b3c37a9d69336cce9f
Sayeed Afridi <as.afridi563@gmail.com>
2023-02-17 03:09:17 +0600

    feat(database): user address and seed data with scripts
    - added user address, foreign address, nationality
    - added related seed data
    - added usefull scripts

--- packages/database/seed.js
+++ packages/database/seed.js
@@ -1,6 +1,67 @@
 /* eslint-disable no-console */
 const { PrismaClient } = require('@prisma/client');
 const { faker } = require('@faker-js/faker');
+const { districts, divisions, unions, upazillas } = require('bd-geojs');
+
+const password = '$2b$10$y5BbyfeOzbJO/LmXgBQxbud8PlEv6jncXQwsqwHG4ONn2ZOmQaWvi'; // admin123
+
+const getRandomNumber = max => faker.datatype.number({ max, min: 0 });
+
+const getRandomBDAddress = () => {
+  const division = divisions[getRandomNumber(divisions.length - 1)];
+  const filteredDistrict = districts.filter(d => d.division_id === division.id);
+  const district =
+    filteredDistrict[getRandomNumber(filteredDistrict.length - 1)];
+  const filteredUpazilla = upazillas.filter(u => u.district_id === district.id);
+  const upazilla =
+    filteredUpazilla[getRandomNumber(filteredUpazilla.length - 1)];
+  const filteredUnion = unions.filter(up => up.upazilla_id === upazilla.id);
+  const union = filteredUnion[getRandomNumber(filteredUnion.length - 1)];
+
+  return {
+    village: union.name,
+    union: union.name,
+    upazilla: upazilla.name,
+    district: district.name,
+    division: division.name,
+    zip: +faker.random.numeric(4),
+  };
+};
+
+const createMyAccount = role => ({
+  uid: `sayeed_${role}`,
+  name: 'Sayeed Afridi',
+  email: `sayeed@${role}.com`,
+  sex: 'male',
+  password,
+  avatar: faker.internet.avatar(),
+  dateOfBirth: faker.date.birthdate(),
+  role,
+  mobile: '01722351346',
+  address: {
+    create: {
+      village: 'Gobindapur',
+      division: 'Rangpur',
+      district: 'Dinajpur',
+      zip: 5230,
+      union: 'Alokjhari',
+      upazilla: 'Khansama',
+    },
+  },
+});
+
+const getRandomBDMobileNumber = () => faker.phone.number('017########');
+
+const getRandomForeignAddress = () => {
+  const country = faker.address.country();
+  const state = faker.address.state();
+  const city = faker.address.city();
+  const street = faker.address.streetAddress();
+  const zip = faker.address.zipCodeByState(state);
+  const mobile = faker.phone.number('+## 9# ### ## ##');
+
+  return { street, city, state, country, mobile, zip };
+};
 
 const createRandomUser = () => {
   const sex = faker.name.sexType();
@@ -10,19 +71,36 @@ const createRandomUser = () => {
   return {
     name,
     sex,
-    password: '$2b$10$Ptt2c9nz.cAgxKCFl32Qa.T/wPuak5grXOSH9QoegvZ0LSA28SBs6',
+    password,
     avatar: faker.internet.avatar(),
     email: faker.helpers
       .unique(faker.internet.email, [firstName, lastName, 'hstu.ac.bd'])
       .toLowerCase(),
     dateOfBirth: faker.date.birthdate(),
+    mobile: getRandomBDMobileNumber(),
   };
 };
 
-const createStudent = () => {
+const createBDStudent = () => {
   return {
     uid: faker.datatype.uuid(),
     ...createRandomUser(),
+    address: {
+      create: getRandomBDAddress(),
+    },
+  };
+};
+
+const createForeignStudent = () => {
+  const foreignAddress = getRandomForeignAddress();
+  return {
+    uid: faker.datatype.uuid(),
+    ...createRandomUser(),
+    nationality: foreignAddress.country,
+    nationalityType: 'foreign',
+    foreignAddress: {
+      create: foreignAddress,
+    },
   };
 };
 
@@ -30,12 +108,39 @@ const superAdmin = {
   uid: 'hstusuperadmin',
   name: 'Super Admin',
   sex: 'male',
-  password: '$2b$10$Ptt2c9nz.cAgxKCFl32Qa.T/wPuak5grXOSH9QoegvZ0LSA28SBs6',
+  password,
   avatar: faker.internet.avatar(),
-  email: 'super@hstu.ac.bd',
+  email: 'superadmin@hstu.ac.bd',
   dateOfBirth: faker.date.birthdate(),
   role: 'superadmin',
+  mobile: getRandomBDMobileNumber(),
+  address: {
+    create: {
+      village: 'Basher Hat',
+      division: 'Rangpur',
+      district: 'Dinajpur',
+      zip: 5200,
+      union: 'Basher Hat',
+      upazilla: 'Dinajpur Sadar',
+    },
+  },
 };
+
+const admin = {
+  uid: 'hstuadmin',
+  name: 'Admin',
+  sex: 'male',
+  password,
+  avatar: faker.internet.avatar(),
+  email: 'admin@hstu.ac.bd',
+  dateOfBirth: faker.date.birthdate(),
+  role: 'admin',
+  mobile: faker.phone.number('017########'),
+  address: {
+    create: getRandomBDAddress(),
+  },
+};
+
 const createTeacher = () => {
   return {
     uid: faker.datatype.uuid(),
@@ -45,32 +150,99 @@ const createTeacher = () => {
 };
 
 const prisma = new PrismaClient();
+
 async function main() {
+  await createMyAccounts();
+  console.log('creating admin & superadmin');
   await prisma.user.upsert({
-    where: { email: 'super@hstu.ac.bd' },
+    where: { email: superAdmin.email },
     update: {},
     create: superAdmin,
   });
+  await prisma.user.upsert({
+    where: { email: admin.email },
+    update: {},
+    create: admin,
+  });
 
-  for (let i = 0; i < 100; i++) {
-    const user = createStudent();
-    const createdUser = await prisma.user.upsert({
+  console.log('creating bangladeshi student');
+  for (let i = 0; i < 10; i++) {
+    const user = createBDStudent();
+    await prisma.user.upsert({
       where: { email: user.email },
       update: {},
       create: user,
+      include: {
+        address: true,
+        foreignAddress: true,
+      },
     });
-    console.log(createdUser);
   }
+
+  console.log('creating foreign students...');
+  for (let i = 0; i < 10; i++) {
+    const user = createForeignStudent();
+    await prisma.user.upsert({
+      where: { email: user.email },
+      update: {},
+      create: user,
+      include: {
+        address: true,
+        foreignAddress: true,
+      },
+    });
+  }
+
+  console.log('creating teachers...');
   for (let i = 0; i < 20; i++) {
     const user = createTeacher();
-    const createdUser = await prisma.user.upsert({
+    await prisma.user.upsert({
       where: { email: user.email },
       update: {},
       create: user,
     });
-    console.log(createdUser);
   }
 }
+
+const createMyAccounts = async () => {
+  console.log('creating my account...');
+  await prisma.user.upsert({
+    where: { email: 'sayeed@superadmin.com' },
+    update: {},
+    create: createMyAccount('superadmin'),
+  });
+  await prisma.user.upsert({
+    where: { email: 'sayeed@admin.com' },
+    update: {},
+    create: createMyAccount('admin'),
+  });
+  await prisma.user.upsert({
+    where: { email: 'sayeed@student.com' },
+    update: {},
+    create: createMyAccount('student'),
+  });
+  await prisma.user.upsert({
+    where: { email: 'sayeed@alumni.com' },
+    update: {},
+    create: createMyAccount('alumni'),
+  });
+  await prisma.user.upsert({
+    where: { email: 'sayeed@stuff.com' },
+    update: {},
+    create: createMyAccount('stuff'),
+  });
+  await prisma.user.upsert({
+    where: { email: 'sayeed@beneficiary.com' },
+    update: {},
+    create: createMyAccount('beneficiary'),
+  });
+  await prisma.user.upsert({
+    where: { email: 'sayeed@teacher.com' },
+    update: {},
+    create: createMyAccount('teacher'),
+  });
+};
+
 main()
   .then(async () => {
     await prisma.$disconnect();
