0ca7d898adb7ee91e67e30b0f7a5daea2e9300ec
zafor <codemen.zafor@gmail.com>
2023-03-05 04:58:48 +0600

    faculty api are created

--- apps/api/src/modules/faculty/faculty.controller.ts
+++ apps/api/src/modules/faculty/faculty.controller.ts
@@ -1,12 +1,54 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  // eslint-disable-next-line prettier/prettier
+  Post,
+} from '@nestjs/common';
+import { Faculty, Prisma } from 'database';
 import { FacultyService } from './faculty.service';
 
-@Controller('faculty')
+@Controller('faculties')
 export class FacultyController {
   // eslint-disable-next-line prettier/prettier
   constructor(private service: FacultyService) {}
+
+  // @Get()
+  // async getFaculties() {
+  //   return this.service.getFaculties();
+  // }
+
   @Get()
-  async getFaculties() {
-    return this.service.getFaculties();
+  async getUsers() {
+    return this.service.findMany();
+  }
+
+  @Get(':id')
+  async getFaculty(@Param('id', ParseIntPipe) id): Promise<Faculty> {
+    return this.service.findOne({ where: { id: id } });
+  }
+
+  @Patch(':id')
+  async updateUser(
+    @Param() params,
+    @Body() data: Prisma.FacultyCreateInput,
+  ): Promise<Faculty> {
+    return this.service.update(+params.id, data);
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param() params) {
+    return this.service.deleteOne(+params.id);
+  }
+
+  @Post('/create')
+  async createFaculty(
+    @Body() data: Prisma.FacultyCreateInput,
+  ): Promise<Faculty> {
+    return this.service.create(data);
   }
 }
--- apps/api/src/modules/faculty/faculty.service.ts
+++ apps/api/src/modules/faculty/faculty.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { Faculty, Prisma } from 'database';
+import { PrismaService } from '../persistance/prisma/prisma.service';
 
 @Injectable()
 export class FacultyService {
-  private readonly faculties = [];
-  getFaculties() {
-    return this.faculties;
+  // eslint-disable-next-line prettier/prettier
+  constructor(private prisma: PrismaService) {}
+
+  async findOne(args: Prisma.UserFindUniqueOrThrowArgs): Promise<Faculty> {
+    return this.prisma.faculty.findUniqueOrThrow({
+      where: { id: args.where.id },
+    });
+  }
+
+  async create(data: Prisma.FacultyCreateInput): Promise<Faculty> {
+    return this.prisma.faculty.create({ data });
+  }
+
+  async update(id: number, data: Prisma.FacultyUpdateInput) {
+    return this.prisma.faculty.update({ where: { id }, data });
+  }
+
+  async findMany(args?: Prisma.FacultyFindManyArgs): Promise<Faculty[]> {
+    return this.prisma.faculty.findMany(args);
+  }
+
+  async deleteOne(id: number) {
+    return this.prisma.faculty.delete({ where: { id } });
   }
 }
