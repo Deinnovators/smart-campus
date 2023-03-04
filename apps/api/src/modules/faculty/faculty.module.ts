9f041e075a532144755de41d8b1469ae37c62421
zafor <codemen.zafor@gmail.com>
2023-03-05 04:58:48 +0600

    faculty api are created

--- apps/api/src/modules/faculty/faculty.module.ts
+++ apps/api/src/modules/faculty/faculty.module.ts
@@ -1,11 +1,13 @@
 import { FacultyService } from '@api/modules/faculty/faculty.service';
 import { Module } from '@nestjs/common';
+import { PrismaService } from '../persistance/prisma/prisma.service';
 import { FacultyController } from './faculty.controller';
 
 @Module({
   imports: [],
-  providers: [FacultyService],
+  providers: [FacultyService, PrismaService],
   controllers: [FacultyController],
+  exports: [FacultyService],
 })
 // eslint-disable-next-line prettier/prettier
 export class FacultyModule {}
