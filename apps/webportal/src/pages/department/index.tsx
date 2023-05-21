56806206430c0bc4b6bde2365a610b5ad3ca685a
Samsuddin-Shohan <shohan.cse.hstu@gmail.com>
2023-05-22 00:05:12 +0600

    faculty & department management

---
+++ apps/webportal/src/pages/department/index.tsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import { Button, Container, TextField, Typography } from '@mui/material';
+import Head from 'next/head';
+export default function DepartmentModule() {
+  const [name, setName] = useState('');
+  const [deanId, setDeanId] = useState(0);
+  const [deanMessage, setDeanMessage] = useState('');
+  const [departments, setDepartments] = useState([]);
+  const handleNameChange = event => {
+    setName(event.target.value);
+  };
+
+  const handleDeanIdChange = event => {
+    setDeanId(event.target.value);
+  };
+
+  const handleDeanMessageChange = event => {
+    setDeanMessage(event.target.value);
+  };
+
+  const handleAddDepartment = event => {
+    event.preventDefault();
+    const departmentName = event.target.elements.departmentName.value;
+    setDepartments([...departments, departmentName]);
+    event.target.reset();
+  };
+  return (
+    <Container>
+      <Head>
+        <title>Modules Registry</title>
+      </Head>
+      <form onSubmit={handleAddDepartment}>
+        <TextField
+          label='Name'
+          variant='outlined'
+          margin='normal'
+          fullWidth
+          value={name}
+          onChange={handleNameChange}
+        />
+        <TextField
+          label='Dean ID'
+          variant='outlined'
+          margin='normal'
+          type='number'
+          fullWidth
+          value={deanId}
+          onChange={handleDeanIdChange}
+        />
+        <TextField
+          label='Dean Message'
+          variant='outlined'
+          margin='normal'
+          fullWidth
+          multiline
+          rows={4}
+          value={deanMessage}
+          onChange={handleDeanMessageChange}
+        />
+        <div>
+          <Typography variant='h6' gutterBottom>
+            Departments:
+          </Typography>
+
+          <TextField
+            label='Department'
+            variant='outlined'
+            margin='normal'
+            name='departmentName'
+          />
+          <Button
+            style={{ display: 'block' }}
+            variant='contained'
+            color='primary'
+            type='submit'>
+            Add Department
+          </Button>
+        </div>
+      </form>
+    </Container>
+  );
+}
---
+++ apps/webportal/src/pages/faculty/edit/[id]/index.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import { Button, Container, TextField } from '@mui/material';
+import Head from 'next/head';
+export default function EditFaculty() {
+  const [name, setName] = useState('');
+  const [deanId, setDeanId] = useState(0);
+  const [deanMessage, setDeanMessage] = useState('');
+  const [departments, setDepartments] = useState([]);
+  const handleNameChange = event => {
+    setName(event.target.value);
+  };
+
+  const handleDeanIdChange = event => {
+    setDeanId(event.target.value);
+  };
+
+  const handleDeanMessageChange = event => {
+    setDeanMessage(event.target.value);
+  };
+
+  const handleAddDepartment = event => {
+    event.preventDefault();
+    const departmentName = event.target.elements.departmentName.value;
+    setDepartments([...departments, departmentName]);
+    event.target.reset();
+  };
+  return (
+    <Container>
+      <Head>
+        <title>Modules Registry</title>
+      </Head>
+      <form onSubmit={handleAddDepartment}>
+        <TextField
+          label='Name'
+          variant='outlined'
+          margin='normal'
+          fullWidth
+          value={name}
+          onChange={handleNameChange}
+        />
+        <TextField
+          label='Dean ID'
+          variant='outlined'
+          margin='normal'
+          type='number'
+          fullWidth
+          value={deanId}
+          onChange={handleDeanIdChange}
+        />
+        <TextField
+          label='Dean Message'
+          variant='outlined'
+          margin='normal'
+          fullWidth
+          multiline
+          rows={4}
+          value={deanMessage}
+          onChange={handleDeanMessageChange}
+        />
+        <div>
+          <Button
+            style={{ display: 'block' }}
+            variant='contained'
+            color='primary'
+            type='submit'>
+            Save
+          </Button>
+        </div>
+      </form>
+    </Container>
+  );
+}
