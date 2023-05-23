2e0efe36b398ef4335ee481b8d868cb85230f3f2
Sayeed Afridi <as.afridi563@gmail.com>
2023-05-23 20:42:28 +0600

    chore(eslint): remove unused var rules

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -8,7 +8,6 @@ module.exports = {
     quotes: [2, 'single', 'avoid-escape'],
     'no-undef': 'error',
     'no-console': 'error',
-    'no-unused-vars': 'error',
     'prettier/prettier': [
       'error',
       {
