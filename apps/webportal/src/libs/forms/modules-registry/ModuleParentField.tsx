b14d216f0712d52e6a719c35d90e8143a2a97622
Sayeed Afridi <as.afridi563@gmail.com>
2023-05-31 18:06:16 +0600

    feat(webportal): search select component

--- apps/webportal/src/libs/forms/modules-registry/ModuleParentField.tsx
+++ apps/webportal/src/libs/forms/modules-registry/ModuleParentField.tsx
@@ -8,13 +8,16 @@ import {
   TextField,
   Zoom,
 } from '@mui/material';
+import SearchSelect, {
+  SearchSelectProps,
+} from '@webportal/libs/forms/shared/SearchSelect';
 import { debounce } from '@webportal/libs/utils/debounce';
 import { api } from '@webportal/services';
 import { ModuleRegistry } from 'database';
 import React, { useCallback, useEffect, useState } from 'react';
 
 export interface ModuleParentFieldProps
-  extends Omit<OutlinedTextFieldProps, 'onChange' | 'onFocus'> {
+  extends Omit<SearchSelectProps, 'onSelect' | 'options'> {
   // eslint-disable-next-line no-unused-vars
   onSelectParent: (value?: string) => void;
 }
@@ -22,17 +25,10 @@ export interface ModuleParentFieldProps
 const ModuleParentField: React.FC<ModuleParentFieldProps> = ({
   onSelectParent,
   value,
-  onBlur,
   ...props
 }) => {
-  const [isEditing, setIsEditing] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
   const [parents, setParents] = useState<ModuleRegistry[]>([]);
-  const [text, setText] = useState<string>('');
-  const valueText = React.useMemo(() => {
-    const parent = parents.find(p => p.url === value);
-    return parent?.name;
-  }, [parents, value]);
 
   const getParents = useCallback(async (name: string) => {
     try {
@@ -53,58 +49,15 @@ const ModuleParentField: React.FC<ModuleParentFieldProps> = ({
     }
   }, []);
 
-  const debouncedGetParents = React.useMemo(() => {
-    return debounce(getParents);
-  }, [getParents]);
-
-  useEffect(() => {
-    debouncedGetParents(text);
-  }, [debouncedGetParents, text]);
-
   return (
-    <FormControl
-      fullWidth
-      margin='normal'
-      style={{ position: 'relative', overflow: 'visible' }}>
-      <TextField
-        value={isEditing ? text : valueText}
-        onFocus={() => setIsEditing(true)}
-        onChange={e => setText(e.target.value)}
-        onBlur={e => {
-          onBlur?.(e);
-        }}
-        {...props}
-      />
-      <Zoom in={isEditing}>
-        <Box pt={1} position='absolute' width='100%' top={48} zIndex={999999}>
-          {loading ? <LinearProgress /> : null}
-          <Paper elevation={5}>
-            <MenuItem
-              key={''}
-              onClick={() => {
-                setText('');
-                setIsEditing(false);
-                onSelectParent?.('');
-              }}
-              sx={{ padding: 2 }}>
-              None
-            </MenuItem>
-            {parents.map(p => (
-              <MenuItem
-                key={p.url}
-                onClick={() => {
-                  setText('');
-                  setIsEditing(false);
-                  onSelectParent?.(p.url);
-                }}
-                sx={{ padding: 2 }}>
-                {p.name}
-              </MenuItem>
-            ))}
-          </Paper>
-        </Box>
-      </Zoom>
-    </FormControl>
+    <SearchSelect
+      onSelect={onSelectParent}
+      loading={loading}
+      value={value}
+      onSearch={getParents}
+      options={parents.map(p => ({ title: p.name, value: p.url }))}
+      {...props}
+    />
   );
 };
 
