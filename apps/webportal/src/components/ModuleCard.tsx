8f7d6e3df9ff3295716472cd4c636ba597fb0e57
Sayeed Afridi <as.afridi563@gmail.com>
2023-10-15 01:12:11 +0600

    feat(webportal): transport page design

--- apps/webportal/src/components/ModuleCard.tsx
+++ apps/webportal/src/components/ModuleCard.tsx
@@ -13,11 +13,11 @@ export const ModuleCard: React.FC<ModuleCardProps> = ({ module }) => {
   const link = module.url.includes('/') ? module.url : `/modules/${module.url}`;
   return (
     <Link href={link}>
-      <Card sx={{ margin: 2 }}>
-        <Box height={200} width={200} position='relative' overflow='hidden'>
+      <Card sx={{ margin: 2, padding: 1 }}>
+        <Box height={180} width={180} position='relative' overflow='hidden'>
           <Image src={getModuleImageUrl(module.icon)} fill alt={module.name} />
         </Box>
-        <Typography style={{ padding: 8 }} variant='h6'>
+        <Typography py={1} style={{ textAlign: 'center' }} variant='h6'>
           {module.name}
         </Typography>
       </Card>
---
+++ apps/webportal/src/pages/transports/index.tsx
@@ -0,0 +1,98 @@
+import React, { FC, ReactNode } from 'react';
+import { Box, Container, Typography } from '@mui/material';
+import Head from 'next/head';
+import { api } from '@webportal/services';
+import { ModuleRegistry } from 'database';
+import { GetServerSideProps } from 'next';
+import { useAppTheme } from '@webportal/libs/hooks';
+import { Spacer } from '@webportal/components';
+import { ArrowRight } from '@mui/icons-material';
+
+export const getServerSideProps: GetServerSideProps = async ctx => {
+  const modules = await api.modules.getAllModules({
+    headers: {
+      [api.authHeaderKey]: `${api.authTokenType} ${ctx.req.cookies.token}`,
+    },
+  });
+  return {
+    props: {
+      modules,
+    },
+  };
+};
+const stoppages = [
+  'Campus',
+  'College Mor',
+  'Terminal',
+  'Moharaja Mor',
+  'Shahi Mosjid Mor',
+  'Shahid Minar Mor',
+  'Sadar Hospital',
+  'Boro Math',
+];
+
+export default function Transports(props: { modules: ModuleRegistry[] }) {
+  const { theme } = useAppTheme();
+  return (
+    <Container>
+      <Head>
+        <title>Transports</title>
+      </Head>
+
+      <OutlineBox title='Ongoing'>
+        <Box display='flex' alignItems='center'>
+          <Box
+            mr={2}
+            border={2}
+            height={38}
+            width={38}
+            borderRadius={19}
+            display='flex'
+            justifyContent='center'
+            alignItems='center'>
+            <Typography variant='h6'>13</Typography>
+          </Box>
+          <Box display='flex' alignItems='center'>
+            {stoppages.map((st, i) => {
+              const isActive = st === 'Terminal';
+              return (
+                <>
+                  <Typography
+                    color={isActive ? 'green' : undefined}
+                    fontSize={isActive ? 24 : undefined}
+                    fontWeight={isActive ? 'bold' : undefined}>
+                    {st}
+                  </Typography>
+                  {i < stoppages.length - 1 ? <ArrowRight /> : null}
+                </>
+              );
+            })}
+          </Box>
+        </Box>
+      </OutlineBox>
+      <OutlineBox title='Upcoming'></OutlineBox>
+    </Container>
+  );
+}
+
+const OutlineBox: FC<{ title: string; children?: ReactNode }> = ({
+  children,
+  title,
+}) => {
+  const { theme } = useAppTheme();
+  return (
+    <Box
+      position='relative'
+      mt={12}
+      p={4}
+      sx={{ border: '2px solid grey', borderRadius: 3 }}>
+      <Box
+        position='absolute'
+        top={-18}
+        bgcolor={theme.palette.background.paper}>
+        <Typography variant='h6'>{title}</Typography>
+      </Box>
+      {children}
+    </Box>
+  );
+};
