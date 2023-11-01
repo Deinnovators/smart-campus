62c6f12ba73d590e2b5cf29c5a21802be1c86b65
Sayeed Afridi <as.afridi563@gmail.com>
2023-11-01 19:11:37 +0600

    feat(api): transport's necessary endpoints

--- apps/api/src/modules/transports/transports.controller.ts
+++ apps/api/src/modules/transports/transports.controller.ts
@@ -1,12 +1,50 @@
 import { TransportsService } from '@api/modules/transports/transports.service';
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { Prisma } from 'database';
 
 @Controller('transports')
 export class TransportsController {
   constructor(private readonly service: TransportsService) {}
 
   @Get('/schedules')
-  async getAllSchedules() {
+  getAllSchedules() {
     return this.service.getAllSchedule();
+  }
+
+  @Get('/drivers')
+  getAllDriverNumbers() {
+    return this.service.getAllDriverNumbers();
+  }
+
+  @Get('/ongoing-upcoming')
+  getOngoingUpcoming() {
+    return this.service.getOngoingUpcoming();
+  }
+
+  @Post('/trips')
+  createTrip(@Body() data: Prisma.TripCreateInput) {
+    return this.service.createTrip(data);
+  }
+
+  @Patch('/trips/:id')
+  updateTrip(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() data: Prisma.TripUpdateInput,
+  ) {
+    return this.service.updateTrip(id, data);
+  }
+
+  @Delete('/trips/:id')
+  deleteTrip(@Param('id', ParseIntPipe) id: number) {
+    return this.service.deleteTrip(id);
   }
 }
--- apps/api/src/modules/transports/transports.service.ts
+++ apps/api/src/modules/transports/transports.service.ts
@@ -1,13 +1,52 @@
 import { PrismaService } from '@api/modules/persistance/prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
+import { Prisma } from 'database';
 
 @Injectable()
 export class TransportsService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async getAllSchedule() {
+  getAllSchedule() {
     return this.prisma.transportSchedule.findMany({
       include: { transport: true },
     });
+  }
+
+  getAllDriverNumbers() {
+    return this.prisma.driverNumbers.findMany();
+  }
+
+  async getOngoingUpcoming() {
+    const date = new Date();
+    const dateString = date.toISOString();
+    const finalDate = new Date(`1970-01-01T${dateString.split('T')[1]}`);
+
+    const upcoming = await this.prisma.transportSchedule.findMany({
+      include: { transport: true },
+      where: {
+        time: {
+          gte: finalDate,
+        },
+      },
+      take: 8,
+    });
+    const ongoing = await this.prisma.trip.findMany();
+
+    return {
+      ongoing,
+      upcoming,
+    };
+  }
+
+  createTrip(data: Prisma.TripCreateInput) {
+    return this.prisma.trip.create({ data });
+  }
+
+  updateTrip(id: number, data: Prisma.TripUpdateInput) {
+    return this.prisma.trip.update({ where: { id }, data });
+  }
+
+  deleteTrip(id: number) {
+    return this.prisma.trip.delete({ where: { id } });
   }
 }
